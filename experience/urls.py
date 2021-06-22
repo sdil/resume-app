278ec6d608d6447244faac17e33bfcbb25e52268
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2021-06-22 11:37:47 +0800

    Refactor to use function-based views

--- experience/urls.py
+++ experience/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 
 from .views import (
-    ExperienceListView,
-    ExperienceCreateView,
-    ExperienceUpdateView,
-    ExperienceDeleteView,
+    main,
+    dashboard,
+    create,
+    update,
+    delete,
 )
 
 urlpatterns = [
-    path("", ExperienceListView.as_view(), name="experience-list"),
-    path("add/", ExperienceCreateView.as_view(), name="experience-add"),
-    path("<int:pk>/", ExperienceUpdateView.as_view(), name="experience-update"),
-    path("<int:pk>/delete/", ExperienceDeleteView.as_view(), name="experience-delete"),
+    path("dashboard", dashboard, name="dashboard"),
+    path("", main, name="experience-list"),
+    path("add/", create, name="experience-add"),
+    path("<int:pk>/", update, name="experience-update"),
+    path("<int:pk>/delete/", delete, name="experience-delete"),
 ]
--- experience/views.py
+++ experience/views.py
@@ -1,26 +1,50 @@
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Experience
-from django.views.generic import ListView
 from .forms import ExperienceForm
+from django.contrib import messages
 
 
-class ExperienceListView(ListView):
-    model = Experience
+def main(request):
+    experiences = Experience.objects.all().order_by("start_date")
+    return render(
+        request, "experience/experience_list.html", {"experiences": experiences},
+    )
 
 
-class ExperienceCreateView(CreateView):
-    model = Experience
-    form_class = ExperienceForm
-    success_url = reverse_lazy("experience-list")
+def dashboard(request):
+    experiences = Experience.objects.all().order_by("start_date")
+    return render(request, "experience/dashboard.html", {"experiences": experiences},)
 
 
-class ExperienceUpdateView(UpdateView):
-    model = Experience
-    form_class = ExperienceForm
-    success_url = reverse_lazy("experience-list")
+def create(request):
+    if request.method == "GET":
+        form = ExperienceForm()
+        return render(request, "experience/experience_form.html", {"form": form,},)
+    else:
+        form = ExperienceForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            messages.error(request, "The form is invalid")
+        return redirect("dashboard")
 
 
-class ExperienceDeleteView(DeleteView):
-    model = Experience
-    success_url = reverse_lazy("experience-list")
+def update(request, pk):
+    experience = get_object_or_404(Experience, id=pk)
+
+    if request.method == "GET":
+        form = ExperienceForm(instance=experience)
+        return render(request, "experience/experience_form.html", {"form": form,},)
+    elif request.method == "POST":
+        form = ExperienceForm(request.POST, instance=experience)
+        if form.is_valid():
+            form.save()
+
+    return redirect("dashboard")
+
+
+def delete(request, pk):
+    experience = get_object_or_404(Experience, id=pk)
+    experience.delete()
+    return redirect("dashboard")
