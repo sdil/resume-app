10a93ec4723dfceeac6487d145cad043a8b4a3eb
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2021-06-22 11:14:09 +0800

    update general layout

--- experience/models.py
+++ experience/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class Experience(models.Model):
-    titile = models.CharField(max_length=100, null=False)
+    title = models.CharField(max_length=100, null=False)
     company = models.CharField(max_length=100, null=False)
-    start_year = models.DateTimeField()
-    end_year = models.DateTimeField()
+    description = models.TextField(blank=True, max_length=300)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
